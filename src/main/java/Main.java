43a29f70a423d52b907cbf69eeeee77e80b95678
omar bakr <omar.bakr.ext@siemens.com>
2023-03-27 16:12:37 +0200

    Stage1:Bind to port

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,24 +8,24 @@ public class Main {
     System.out.println("Logs from your program will appear here!");
 
     //  Uncomment this block to pass the first stage
-    //    ServerSocket serverSocket = null;
-    //    Socket clientSocket = null;
-    //    int port = 6379;
-    //    try {
-    //      serverSocket = new ServerSocket(port);
-    //      serverSocket.setReuseAddress(true);
-    //      // Wait for connection from client.
-    //      clientSocket = serverSocket.accept();
-    //    } catch (IOException e) {
-    //      System.out.println("IOException: " + e.getMessage());
-    //    } finally {
-    //      try {
-    //        if (clientSocket != null) {
-    //          clientSocket.close();
-    //        }
-    //      } catch (IOException e) {
-    //        System.out.println("IOException: " + e.getMessage());
-    //      }
-    //    }
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 6379;
+        try {
+          serverSocket = new ServerSocket(port);
+          serverSocket.setReuseAddress(true);
+          // Wait for connection from client.
+          clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+          System.out.println("IOException: " + e.getMessage());
+        } finally {
+          try {
+            if (clientSocket != null) {
+              clientSocket.close();
+            }
+          } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+          }
+        }
   }
 }
